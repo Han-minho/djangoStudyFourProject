8d43e986632cabd37ce95679d3fbcdab432d5015
Hanminho <hihi6024@gmail.com>
2023-09-11 15:44:28 +0900

    서식 변경

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ['id', 'username', 'first_name',
-                  'last_name', 'bio', 'avatar', 'email',
+        fields = ['id', 'username', 'first_name', 'last_name',
+                  'bio', 'avatar', 'email',
                   'is_active', 'created', 'updated']
         read_only_field = ['is_active']
