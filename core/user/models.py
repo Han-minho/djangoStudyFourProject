5cd0f4d93606d568946ce9f8ac01e06d6471f5c3
Hanminho <hihi6024@gmail.com>
2023-09-11 13:54:06 +0900

    core.user UserManager 인식을 위한 위치 변경

--- core/user/models.py
+++ core/user/models.py
@@ -4,28 +4,8 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.http import Http404
 from django.db import models
 
+
 # Create your models here.
-
-
-class User(AbstractBaseUser, PermissionsMixin):
-    public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
-    username = models.CharField(db_index=True, max_length=255, unique=True)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(db_index=True, unique=True)
-    is_active = models.BooleanField(default=True)
-    is_superuser = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField(auto_now_add=True)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
-
-    def __str__(self):
-        return f'{self.email}'
-
-    @property
-    def name(self):
-        return f"{self.first_name} {self.last_name}"
 
 
 class UserManager(BaseUserManager):
@@ -44,9 +24,9 @@ class UserManager(BaseUserManager):
         if username is None:
             raise TypeError('Users must have a username.')
         if email is None:
-            raise  TypeError('Users must have a emil.')
+            raise TypeError('Users must have a emil.')
         if password is None:
-            raise TypeError('User must have an email.')
+            raise TypeError('User must have an Password.')
         user = self.model(username=username, email=self.normalize_email(email), **kwargs)
         user.set_password(password)
         user.save(using=self._db)
@@ -67,3 +47,25 @@ class UserManager(BaseUserManager):
         user.is_staff = True
         user.save(using=self._db)
         return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    object = UserManager()
+
+    def __str__(self):
+        return f'{self.email}'
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
