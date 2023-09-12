dbc216af9a938cfdd8d96ef4c50c37ef22c34576
Hanminho <hihi6024@gmail.com>
2023-09-12 14:17:35 +0900

    모델 추상화로 인한 모델 수정

--- core/user/models.py
+++ core/user/models.py
@@ -52,17 +52,19 @@ class UserManager(BaseUserManager, AbstractManager):
 
 
 class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
-    public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
+    # public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
     username = models.CharField(db_index=True, max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_superuser = models.BooleanField(default=False)
+
     bio = models.TextField(null=True)
     avatar = models.ImageField(null=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+
+    # created = models.DateTimeField(auto_now_add=True)
+    # updated = models.DateTimeField(auto_now=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     objects = UserManager()
