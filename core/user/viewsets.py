c05b62ee33922f48550bb1dcc48d4ff309734aa4
Hanminho <hihi6024@gmail.com>
2023-09-12 14:22:36 +0900

    추상화 된 추상 뷰 셋 추가

--- core/user/viewsets.py
+++ core/user/viewsets.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework import viewsets
+
+from core.abstract.viewsets import AbstractViewSet
 from core.user.serializers import UserSerializer
 from core.user.models import User
 
 # Create your views here.
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(AbstractViewSet):
     http_method_names = ('patch', 'get')
     permission_classes = (IsAuthenticated,)
     serializer_class = UserSerializer
