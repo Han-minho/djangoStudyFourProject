ca88c8869408ddf98851e90ea436bd5f3a91e5bb
Hanminho <hihi6024@gmail.com>
2023-09-12 15:04:54 +0900

    AbstractSerializer 중복 코드 제거 및 다른 모델의 지원을 위한 작업

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -4,9 +4,9 @@ from core.abstract.serializers import AbstractSerializer
 
 
 class UserSerializer(AbstractSerializer):
-    id = serializers.UUIDField(source='public_id', read_only=True, format='hex')
-    created = serializers.DateTimeField(read_only=True)
-    updated = serializers.DateTimeField(read_only=True)
+    # id = serializers.UUIDField(source='public_id', read_only=True, format='hex')
+    # created = serializers.DateTimeField(read_only=True)
+    # updated = serializers.DateTimeField(read_only=True)
 
     class Meta:
         model = User
